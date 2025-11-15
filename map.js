cce6a012cbd283de6447d36aeceb6c2b6fd3a991
Albab Neawz <newaz.albab@gmail.com>
2025-11-14 23:20:29 -0800

    Update map.js

--- map.js
+++ map.js
@@ -17,3 +17,25 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
 });
+
+map.on('load', async () => {
+  console.log("Map fully loaded — adding data...");
+
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+  });
+
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+      'line-color': 'green',
+      'line-width': 3,
+      'line-opacity': 0.4
+    }
+  });
+
+  console.log("Bike route layer added.");
+});
